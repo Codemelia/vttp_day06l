90bf2df0eee19037d29eb94c141d10d921137b7b
Codemelia <ameliaw2903@gmail.com>
2024-10-17 10:59:49 +0800

    added Product.java

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 // compile
 // javac -d classes --source-path src src/vttp_day06l/App.java
 // javac -d bin --source-path . ./*.java
+// javac -d bin --source-path . ./*.java day06/*.java
 
 // run
 // java -cp bin App
@@ -9,17 +10,44 @@
 
 // run jar
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import day06.MyImplementation;
+
 public class App {
 
     public static void main(String[] args) {
 
-        Thread th = new Thread(new Runnable() {
+        /* Thread th = new Thread(new Runnable() {
             public void run() {
                 for (int i = 0; i < 10; i++) {
                 System.out.println(Thread.currentThread().getName() + " -> " + i);
                 }
             }
         });
-        th.start();
+        th.start(); */
+
+        MyImplementation my1 = new MyImplementation();
+        MyImplementation my2 = new MyImplementation();
+        MyImplementation my3 = new MyImplementation();
+        MyImplementation my4 = new MyImplementation();
+        MyImplementation my5 = new MyImplementation();
+
+        // Executing tasks using a single thread
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.execute(my1);
+        es.execute(my2);
+        es.shutdown(); // To release memory
+
+        // Execute the tasks using fixed thread pool
+        ExecutorService es1 = Executors.newFixedThreadPool(5);
+        es1.execute(my1);
+        es1.execute(my2);
+        es1.execute(my3);
+        es1.execute(my4);
+        es1.execute(my5);
+        es1.shutdown(); // To release memory
+
     }
 }
